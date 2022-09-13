ccb75050e28a6f5810de8f99f59d050f0b5af07e
wogks <wogks27@naver.com>
2022-09-13 13:56:21 +0900

    Feat: movie api 생성

---
+++ lib/data/data_source/movie_api.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:movie_app/data/data_source/movie_api_imple.dart';
+import 'package:movie_app/domain/model/movie_dto.dart';
+import 'package:http/http.dart' as http;
+
+class MovieApiImpl implements MovieApi {
+  @override
+  Future<MovieDto> getMovieList() async {
+    Uri url = Uri.parse(
+        'https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1');
+
+    final response = await http.get(url);
+
+    String jsonString = response.body;
+
+    Map<String, dynamic> json = jsonDecode(jsonString);
+
+    return MovieDto.fromJson(json);
+  }
+
+  @override
+  Future<MovieDto> getSearchMoviewList(String query) async {
+    Uri url = Uri.parse(
+        'https://api.themoviedb.org/3/search/movie?language=ko-KR&api_key=a64533e7ece6c72731da47c9c8bc691f&query=$query');
+
+    final response = await http.get(url);
+
+    String jsonString = response.body;
+
+    Map<String, dynamic> json = jsonDecode(jsonString);
+
+    return MovieDto.fromJson(json);
+  }
+}
--- lib/data/data_source/movie_api_imple.dart
+++ lib/data/data_source/movie_api_imple.dart
@@ -1,3 +1,6 @@
+import 'package:movie_app/domain/model/movie_dto.dart';
+
 abstract class MovieApi {
-  Future
+  Future<MovieDto> getMovieList();
+  Future<MovieDto> getSearchMoviewList(String query);
 }
